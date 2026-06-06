669461fe5408c07a16572565ead00224e985cb94
habba2707 <kurthabba@gmail.com>
2026-06-06 19:59:25 +1000

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'http://localhost:3000')
-  }
+  plugins: [react()]
 })
